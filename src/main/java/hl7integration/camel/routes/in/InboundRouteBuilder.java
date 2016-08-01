00463d624bcbaed1738ed7c4cca18a7b7c998614
dinkinsh <heath.r.dinkins@leidos.com>
2016-08-01 13:21:20 -0400

    - added validator customization to via env support (so instead of defaulting to localhost... i can set the port and host myself) -  changed endpoint properties endpoint server back to localhost (its just cleaner)

--- src/main/java/hl7integration/camel/routes/in/InboundRouteBuilder.java
+++ src/main/java/hl7integration/camel/routes/in/InboundRouteBuilder.java
@@ -9,10 +9,15 @@ import org.springframework.stereotype.Component;
 public class InboundRouteBuilder extends SpringRouteBuilder {
     private static final Logger log = LoggerFactory.getLogger(InboundRouteBuilder.class);
 
+
+    Processor processor = new Processor();
+    String vHost = (System.getenv("VALIDATOR_SERVICE_HOST")== null) ? processor.getPropValues("endpoint-server") : System.getenv("VALIDATOR_SERVICE_HOST");
+    String vPort = (System.getenv("VALIDATOR_SERVICE_PORT")== null) ? processor.getPropValues("endpoint-port") : System.getenv("VALIDATOR_SERVICE_PORT");
+
     @Override
     public void configure() throws Exception {
 
-        from("mllpport").routeId("Validator-Camel-Route")
+        from("mina:tcp://" + vHost + ":" + vPort + "?sync=true&codec=#hl7codec").routeId("Validator-Camel-Route")
                 .to("bean:processor?method=processMessage")
                 .to("bean:respondACK?method=process")
                 .end();
